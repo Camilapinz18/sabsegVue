b8b9a267effb20174a3f4f677e3e9ce5a677fda7
Camila Pinzon <camilapinz18@gmail.com>
2023-06-09 18:58:34 -0500

    update

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,64 +1,113 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { RouterLink } from 'vue-router';
+import axios from 'axios';
 
+// Data objects for sign-in and sign-up
+const signIn = {
+  email: '',
+  password: '',
+};
+
+const signUp = {
+  name: '',
+  surname: '',
+  phone: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+// Sign-in user
+const signInUser = () => {
+  axios
+    .post('api_url/signin', signIn)
+    .then(response => {
+      // Handle the response
+      console.log(response.data);
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error(error);
+    });
+};
+
+// Register user
+const registerUser = () => {
+    console.log("IMPRIMIR",signUp.name)
+  axios
+    .post('http://localhost:8085/api/v1/customers', signUp)
+    .then(response => {
+      // Handle the response
+      console.log(response.data);
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error(error);
+    });
+};
 </script>
 
 <template>
-    <div class="bg-cont">
-        <div class="login-cont">
-            <div class="inputs">
-                <img class="logo-img" src="../assets/images/logoB.png" />
+  <div class="bg-cont">
+    <div class="login-cont">
+      <div class="inputs">
+        <img class="logo-img" src="../assets/images/logoB.png" />
 
-                <n-card class="tab">
-                    <n-tabs class="card-tabs" default-value="signin" size="large" animated style="margin: 0 -4px"
-                        pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;">
-                        <n-tab-pane name="signin" tab="Iniciar sesión">
-                            <n-form>
-                                <n-form-item-row label="Email">
-                                    <n-input />
-                                </n-form-item-row>
-                                <n-form-item-row label="Contraseña">
-                                    <n-input />
-                                </n-form-item-row>
-                            </n-form>
+        <n-card class="tab">
+          <n-tabs
+            class="card-tabs"
+            default-value="signin"
+            size="large"
+            animated
+            style="margin: 0 -4px"
+            pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;"
+          >
+            <n-tab-pane name="signin" tab="Iniciar sesión">
+              <n-form>
+                <n-form-item-row label="Email">
+                  <n-input v-model="signIn.email" />
+                </n-form-item-row>
+                <n-form-item-row label="Contraseña">
+                  <n-input v-model="signIn.password" type="password" />
+                </n-form-item-row>
+              </n-form>
 
-                            <RouterLink to="/account"><n-button color="#0d0378">
-                                    Iniciar sesión
+              <n-button color="#0d0378" @click="signInUser">
+                Iniciar sesión
+              </n-button>
+            </n-tab-pane>
+            <n-tab-pane name="signup" tab="Registrarse">
+              <n-form>
+                <n-form-item-row label="Nombres">
+                  <n-input v-model="signUp.name" />
+                </n-form-item-row>
+                <n-form-item-row label="Apellidos">
+                  <n-input v-model="signUp.surname" />
+                </n-form-item-row>
+                <n-form-item-row label="Teléfono">
+                  <n-input v-model="signUp.phone" />
+                </n-form-item-row>
+                <n-form-item-row label="Email">
+                  <n-input v-model="signUp.email" />
+                </n-form-item-row>
+                <n-form-item-row label="Contraseña">
+                  <n-input v-model="signUp.password" type="password" />
+                </n-form-item-row>
+                <n-form-item-row label="Confirme contraseña">
+                  <n-input v-model="signUp.confirmPassword" type="password" />
+                </n-form-item-row>
+              </n-form>
 
-                                </n-button></RouterLink>
-
-                        </n-tab-pane>
-                        <n-tab-pane name="signup" tab="Registrarse">
-                            <n-form>
-                                <n-form-item-row label="Nombre completo">
-                                    <n-input />
-                                </n-form-item-row>
-                                <n-form-item-row label="Email">
-                                    <n-input />
-                                </n-form-item-row>
-                                <n-form-item-row label="Contraseña">
-                                    <n-input />
-                                </n-form-item-row>
-                                <n-form-item-row label="Confirme contraseña">
-                                    <n-input />
-                                </n-form-item-row>
-
-                            </n-form>
-
-                            <RouterLink to="/account"><n-button color="#0d0378">
-                                    Registrarse
-
-                                </n-button></RouterLink>
-
-                        </n-tab-pane>
-                    </n-tabs>
-                </n-card>
-
-            </div>
-        </div>
+              <n-button color="#0d0378" @click="registerUser">
+                Registrarse
+              </n-button>
+            </n-tab-pane>
+          </n-tabs>
+        </n-card>
+      </div>
     </div>
+  </div>
 </template>
-
 <style scoped>
 .bg-cont {
     /* background-image: url('../assets/images/login-bg.jpg'); */
@@ -73,7 +122,7 @@ import { RouterLink } from 'vue-router'
 .login-cont {
     width: 40%;
     background-color: white;
-    height: 70%;
+    height: 90%;
     border-radius: 10px;
     display: flex;
     flex-direction: column;
