f2ae1998abbaaf7e9a9fea7a44aebe15f1282575
Camila Pinzon <camilapinz18@gmail.com>
2023-07-01 10:55:28 -0500

    change to local host

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-    apiUrl: 'https://fastapi-production-63d8.up.railway.app/api/v1/'
+    //apiUrl: 'https://fastapi-production-63d8.up.railway.app/api/v1/'
+    apiUrl: 'http://localhost:8000/api/v1/'
 };
